44029b89b6bb690565032a69b410bf6d2e059fa2
Shubham <shuhamkanzariya2013@gmail.com>
2025-06-10 08:05:53 +0530

    Update environment API URLs for development and production to use the new onRender endpoint.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
 
     // apiUrl: 'http://192.168.1.77:5001/api/user/v1'
     // apiUrl: 'http://192.168.1.10:5001/api/user/v1'
-    // apiUrl: 'https://website-developemnt-new.onrender.com/api'
-    apiUrl: 'http://192.168.220.122:5001/api'
+    apiUrl: 'https://website-developemnt-new.onrender.com/api'
+    // apiUrl: 'http://192.168.220.122:5001/api'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.220.122:5001/api' // Replace with your actual API URL
-  // apiUrl: 'https://website-developemnt-new.onrender.com/api' // Replace with your actual API URL
+  // apiUrl: 'http://192.168.220.122:5001/api' // Replace with your actual API URL
+  apiUrl: 'https://website-developemnt-new.onrender.com/api' // Replace with your actual API URL
 };
